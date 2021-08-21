f850d71033b69fb0933f0e5f79c7b373c163ca96
Crowdbotics <team@crowdbotics.com>
2021-08-21 02:50:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "fancy_flower_29998Identifier",
+  url: "https://fancy_flower_29998Identifier.botics.co",
+  api: "https://fancy_flower_29998Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
